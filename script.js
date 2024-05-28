8fde54ae8d96e84ebd8150f30570aad1e487064b
Vivek <vivdave2004@gmail.com>
2024-05-28 14:50:08 +0530

    Final Commit

--- script.js
+++ script.js
@@ -10,13 +10,20 @@ function confirmPassword(str1,str2) {
 const password = document.getElementById("password");
 const confirmpassword = document.getElementById("confirmpassword");
 const submit = document.getElementById("submit");
-submit.addEventListener('click', function() {
+const text_field =document.getElementById("text_field")
+confirmpassword.addEventListener('input', function() {
     confirmation = confirmPassword(password.value,confirmpassword.value);
 
 if (confirmation === true) {
-    alert("Passwords match");
+    text_field.textContent = "Passwords match";
+    text_field.style.color = "green";
+    confirmpassword.style.borderColor = "green";
+    password.style.borderColor = "green";
 }
 else {
-    alert("Passwords don't match")
+    text_field.textContent = "Passwords don't match";
+    text_field.style.color = "red";
+    confirmpassword.style.borderColor = "red";
+    confirmpassword.style.borderColor = "red";
 }
 });
